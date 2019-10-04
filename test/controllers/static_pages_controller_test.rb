8b6355a40ab54a1ef63a36add990e7f5eaa1e7ae
Ryan Walker <rytwalker@gmail.com>
2019-10-03 22:38:17 -0400

    added title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get root" do
+  test 'should get root' do
     get root_url
     assert_response :success
   end
@@ -13,7 +15,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
-    assert_select 'title', "Home | #{@base_title}"
+    assert_select 'title', @base_title.to_s
   end
 
   test 'should get help' do
