cda6cfde16fd869877590cf3a7eaf3d158700ace
Ryan Walker <rytwalker@gmail.com>
2019-10-09 19:05:09 -0400

    added a form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,20 @@
 
 # Controller for creating users
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user]) # not done yet!
+    if @user.save
+      # handle save
+    else
+      render 'new'
+    end
+  end
 end
