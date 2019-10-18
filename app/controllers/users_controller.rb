143ef0c48d8281b0fea2daa81c940d5935da6c54
Ryan Walker <rytwalker@gmail.com>
2019-10-18 04:27:01 -0400

    finished signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) # not done yet!
     if @user.save
-      # handle save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
     else
       render 'new'
     end
