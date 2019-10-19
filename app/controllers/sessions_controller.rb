d61d34555c4b426849c78f4a0a7a55754734c907
Ryan Walker <rytwalker@gmail.com>
2019-10-19 12:52:19 -0400

    added login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,20 @@
+# frozen_string_literal: true
+
+# Sessions controller
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      # login user and redirect to user show page
+    # == user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
-  def destroy
-  end
+  def destroy; end
 end
