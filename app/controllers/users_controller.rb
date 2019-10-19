d067a939de4844f9a6e9c0d66791540ec3cbe319
Ryan Walker <rytwalker@gmail.com>
2019-10-19 13:17:45 -0400

    added logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) # not done yet!
     if @user.save
+      log_in @user
       flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
